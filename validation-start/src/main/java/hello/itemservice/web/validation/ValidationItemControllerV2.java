0ba3ff52ecf6b77b4210f9ab9058c438104cf5b5
swsong <ssw890720@gmail.com>
2022-04-14 00:07:17 +0900

    v2 bindingRequest로 에러 처리

--- validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -3,13 +3,19 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v2/items")
 @RequiredArgsConstructor
@@ -38,7 +44,27 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다."));
+        }
+
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice <= 100000) {
+                //글로벌 에러는 ObjectError 객체로 세팅
+                bindingResult.addError(new ObjectError("item","가격 * 수량의 합은 10000원 이상어햐 합니다 현재값="+resultPrice));
+            }
+        }
+
+        //검증 실패시 처리
+        if (bindingResult.hasErrors()) {
+            //bindingResult는 자동으로 모델에 담기기 때문에 모델에 따로 담을 필요없다.
+            log.info("error={} ", bindingResult);
+            return "validation/v2/addForm";
+        }
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
