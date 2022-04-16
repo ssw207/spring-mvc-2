11502c33dfccf80c3fe61eaaaa06d576f9058cf3
swsong <ssw890720@gmail.com>
2022-04-16 23:54:13 +0900

    오류 발생시 필드값 유지

--- validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -43,7 +43,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItem(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         if (!StringUtils.hasText(item.getItemName())) {
@@ -55,6 +55,39 @@ public class ValidationItemControllerV2 {
             if (resultPrice <= 100000) {
                 //글로벌 에러는 ObjectError 객체로 세팅
                 bindingResult.addError(new ObjectError("item","가격 * 수량의 합은 10000원 이상어햐 합니다 현재값="+resultPrice));
+            }
+        }
+
+        //검증 실패시 처리
+        if (bindingResult.hasErrors()) {
+            //bindingResult는 자동으로 모델에 담기기 때문에 모델에 따로 담을 필요없다.
+            log.info("error={} ", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            //bindingFailure : 바인딩 실패 여부
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+
+        if (item.getPrice() == null || item.getPrice() != null && (item.getPrice() < 1000 || item.getPrice() > 100000)) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1000 ~100,000 사이입니다"));
+        }
+
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice <= 100000) {
+                //글로벌 에러는 ObjectError 객체로 세팅
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10000원 이상어햐 합니다 현재값="+resultPrice));
             }
         }
 
