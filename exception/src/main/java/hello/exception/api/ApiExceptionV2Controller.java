e8d94c05ace966f22064883164a20254e1bd6895
swsong <ssw890720@gmail.com>
2022-05-30 00:12:25 +0900

    Exception Handler 예제

--- exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -4,19 +4,37 @@ import hello.exception.dto.MemberDto;
 import hello.exception.exception.UserException;
 import hello.exception.exhandler.ErrorResult;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 public class ApiExceptionV2Controller {
 
+    /*
+    컨트롤러 내부에서 에러발생시 공통으로 처리한다.
+    컨트롤러 에러발생 -> 익셉션 리졸버 -> 처리후 정상응답
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 상태코드를 바꾸지 않으면 기본적으로 200이 리턴된다.
     @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illeageExHandler(IllegalArgumentException e) {
+    public ErrorResult illeageExHandler(IllegalArgumentException e) { // IllegalArgumentException 자식 예외까지 잡힌다
         log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
+        return new ErrorResult("BAD", e.getMessage()); // 상태코드를 바꾸지 않으면 200리턴
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) { // @ExceptionHandler(UserException.class) 생략
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("BAD", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+    
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) { // 자식예외로 처리되지 않은 예외는 여기서 처리된다.
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부오류");
     }
 
     @GetMapping("/api2/members/{id}")
